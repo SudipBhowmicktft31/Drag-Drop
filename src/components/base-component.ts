09ee7a9a9d6cc16741a7200069abd4120ccf1883
Ankaj Goyal <goyal.ankaj.tft@outlook.com>
2022-11-29 18:17:54 +0530

    updated code

---
+++ src/components/base-component.ts
@@ -0,0 +1,41 @@
+namespace App {
+  //Component base class
+  export abstract class Component<
+    T extends HTMLElement,
+    U extends HTMLElement
+  > {
+    templateElement: HTMLTemplateElement;
+    hostElement: T;
+    element: U;
+
+    constructor(
+      templateId: string,
+      hostElementId: string,
+      insertAtStart: boolean,
+      newElementId?: string
+    ) {
+      this.templateElement = document.getElementById(
+        templateId
+      )! as HTMLTemplateElement;
+      this.hostElement = document.getElementById(hostElementId)! as T;
+
+      const impotedNode = document.importNode(
+        this.templateElement.content,
+        true
+      );
+      this.element = impotedNode.firstElementChild as U;
+      if (newElementId) {
+        this.element.id = newElementId;
+      }
+      this.attach(insertAtStart);
+    }
+    private attach(insertAtBegining: boolean) {
+      this.hostElement.insertAdjacentElement(
+        insertAtBegining ? "afterbegin" : "beforeend",
+        this.element
+      );
+    }
+    abstract configure(): void;
+    abstract renderContent(): void;
+  }
+}
